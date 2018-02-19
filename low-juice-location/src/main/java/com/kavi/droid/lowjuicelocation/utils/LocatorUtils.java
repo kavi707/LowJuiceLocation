4a7574dd49d222da05de453120276d0adaa5a7e9
Kavimal Wijewardana <kavi707@gmail.com>
2018-02-18 23:14:28 -0500

    Added utils classes and Manifest permissions

---
+++ low-juice-location/src/main/java/com/kavi/droid/lowjuicelocation/utils/LocatorUtils.java
@@ -0,0 +1,114 @@
+package com.kavi.droid.lowjuicelocation.utils;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+
+import com.kavi.droid.lowjuicelocation.models.NetworkCellInfo;
+import com.kavi.droid.lowjuicelocation.models.NetworkOperatorInfo;
+
+/**
+ * Created by kavi707 on 2/11/18.
+ * @author Kavimal Wijewardana <kavi707@gmail.com>
+ */
+
+public class LocatorUtils {
+
+    private static LocatorUtils locatorUtils;
+    private Context context = null;
+
+    public static LocatorUtils getInstance(Context context) {
+        if (locatorUtils == null)
+            locatorUtils = new LocatorUtils(context);
+
+        return locatorUtils;
+    }
+
+    private LocatorUtils(Context context) {
+        this.context = context;
+    }
+
+    public NetworkOperatorInfo getOperatorInfo() {
+        NetworkOperatorInfo networkOperatorInfo = null;
+
+        TelephonyManager telephonyManager = (TelephonyManager) context.getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+            String networkData = null;
+            int networkType = 0;
+            if (telephonyManager != null) {
+                networkData = telephonyManager.getSimOperator();
+
+                if (networkData == null)
+                    networkData = telephonyManager.getNetworkOperator();
+
+                networkType = telephonyManager.getNetworkType();
+
+                networkOperatorInfo = new NetworkOperatorInfo(getMCC(networkData),
+                        getMNC(networkData), getNetworkTypeFromCode(networkType));
+            }
+        }
+
+        return networkOperatorInfo;
+    }
+
+    public NetworkCellInfo getCellInfo() {
+
+        GsmCellLocation location;
+        NetworkCellInfo networkCellInfo = null;
+
+        TelephonyManager telephonyManager = (TelephonyManager) context.getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+            if (telephonyManager != null) {
+                location = (GsmCellLocation) telephonyManager.getCellLocation();
+                networkCellInfo = new NetworkCellInfo(location.getCid(), location.getLac());
+            }
+        }
+
+
+        return networkCellInfo;
+    }
+
+    private String getNetworkTypeFromCode(int code) {
+        String networkType = "";
+        switch (code) {
+            case 16:
+                networkType = NetworkOperatorInfo.GSM_TYPE;
+                break;
+            case 4:
+                networkType = NetworkOperatorInfo.CDMA_TYPE;
+                break;
+            case 3:
+                networkType = NetworkOperatorInfo.UMTS_TYPE;
+                break;
+            case 13:
+                networkType = NetworkOperatorInfo.LTE_TYPE;
+                break;
+            case 8:
+                networkType = NetworkOperatorInfo.LTE_TYPE;
+                break;
+            default:
+                networkType = NetworkOperatorInfo.UNKNOWN_TYPE;
+                break;
+        }
+        return networkType;
+    }
+
+    private int getMCC(String networkData) {
+        return Integer.parseInt(networkData.substring(0, 3));
+    }
+
+    private int getMNC(String networkData) {
+        return Integer.parseInt(networkData.substring(3));
+    }
+}

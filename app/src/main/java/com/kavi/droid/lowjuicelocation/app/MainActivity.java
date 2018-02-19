d8fe4950c3c1e23bad17043cfe9c36c2dc1a63c2
Kavimal Wijewardana <kavi707@gmail.com>
2018-02-18 22:33:36 -0500

    Created example project to call library module

---
+++ app/src/main/java/com/kavi/droid/lowjuicelocation/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kavi.droid.lowjuicelocation.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
